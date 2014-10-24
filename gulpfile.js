132de6c72a068df76d7d93b6d4d999deb47fc2bb
Xiyang Chen <settinghead@gmail.com>
2014-10-24 12:02:44 -0400

    risevision.core.systemmessages
    - Also include lint & metrics tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
+
+/*jshint node: true */
+
 var
     gulp = require("gulp"),
-    factory = require("widget-tester").gulpTaskFactory;
+    factory = require("widget-tester").gulpTaskFactory,
+    runSequence = require("run-sequence"),
+    jshint = require("gulp-jshint");
 
 var unitTestFiles = [
   "components/angular/angular.js",
@@ -15,4 +20,19 @@ var unitTestFiles = [
 
 gulp.task("test:unit", factory.testUnitAngular({testFiles: unitTestFiles}));
 gulp.task("test:unit-watch", factory.testUnitAngular({testFiles: unitTestFiles, watch: true}));
-gulp.task("test", ["test:unit"]);
+gulp.task("metrics", factory.metrics());
+
+gulp.task("lint", function() {
+  return gulp.src([
+      "src/js/**/*.js",
+      "test/**/*.js"
+    ])
+    .pipe(jshint())
+    .on("error", function () {
+      process.exit(1);
+    });
+});
+
+gulp.task("test", ["lint"], function (cb) {
+  runSequence("test:unit", "metrics", cb);
+});
